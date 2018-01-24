955d697e49784fba0f90c1a6a970f00631d8225d
ism49186163 <ism49186163@a05.informatica.escoladeltreball.org>
2018-01-24 12:09:35 +0100

    Comentario

---
+++ esentero.py
@@ -0,0 +1,8 @@
+#coding: utf-8
+divisor=input("Indique un divisor:")
+dividendo=input("Indique un dividendo:")
+resultado=divisor%dividendo
+if resultado==0:
+	print "El resultado es entero"
+else:
+	print "El resultado no es entero" 
